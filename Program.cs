0b645ce5e6772f47e3d5c13f25a480502e10ecc3
lavanyaboddu31@gmail.com <lavanyaboddu31@gmail.com>
2023-07-24 18:17:12 +0530

    first commit

--- Program.cs
+++ Program.cs
@@ -1,119 +1,104 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace ConAppAssignment12
+namespace ConAppAssignment14
 {
     internal class Program
     {
+        public static void BubbleSort(int[] arr)
+        {
+            int n = arr.Length;
+            bool swapped;
+            for (int i = 0; i < n - 1; i++)
+            {
+                swapped = false;
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                        swapped = true;
+
+                    }
+                }
+                if (!swapped)
+                {
+                    break;
+                }
+            }
+        }
+        public static void InsertionSort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; i++)
+            {
+                int key = arr[i];
+                int j = i - 1;
+                while (j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+                arr[j + 1] = key;
+            }
+        }
+        public static void Print(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine("\n");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a piece of text:");
-            string inputText = Console.ReadLine();
+            Console.WriteLine("Enter numbers for Bubble Sort");
+            int[] num = new int[5];
+            for (int i = 0; i < num.Length; i++)
+            {
+                num[i] = int.Parse(Console.ReadLine());
 
-            int wordCount = CountWords(inputText);
-            Console.WriteLine($"Word Count: {wordCount}");
+            }
+            Console.Write("Numbers are :");
+            Print(num);
 
-            var emailAddresses = GetEmailAddresses(inputText);
-            if (emailAddresses.Count > 0)
+            Console.WriteLine("Print Array without Sort");
+            Print(num);
+            Stopwatch s1 = new Stopwatch();
+            s1.Start();
+            BubbleSort(num);
+            s1.Stop();
+            Console.WriteLine("print Array after bubble sort");
+            Print(num);
+            Console.WriteLine($"Arraysize is {num.Length} Time Taken for bubble sort {s1.Elapsed.TotalMilliseconds} milliseconds");
+            Console.WriteLine("==========================");
+            Console.WriteLine("Enter numbers for insertion sort");
+            int[] num1 = new int[5];
+            for (int i = 0; i < num1.Length; i++)
             {
-                Console.WriteLine("Email Addresses found:");
-                foreach (var email in emailAddresses)
-                {
-                    Console.WriteLine(email);
-                }
-            }
-            else
-            {
-                Console.WriteLine("No email addresses found.");
-            }
+                num1[i] = int.Parse(Console.ReadLine());
 
-            var mobileNumbers = ExtractMobileNumbers(inputText);
-            if (mobileNumbers.Count > 0)
-            {
-                Console.WriteLine("Mobile Numbers found:");
-                foreach (var number in mobileNumbers)
-                {
-                    Console.WriteLine(number);
-                }
             }
-            else
-            {
-                Console.WriteLine("No mobile numbers found.");
-            }
-
-            Console.WriteLine("Enter a custom regular expression to search:");
-            string customRegexPattern = Console.ReadLine();
-
-            var customRegexMatches = PerformCustomRegexSearch(inputText, customRegexPattern);
-            if (customRegexMatches.Count > 0)
-            {
-                Console.WriteLine("Custom Regex Matches found:");
-                foreach (var match in customRegexMatches)
-                {
-                    Console.WriteLine(match);
-                }
-            }
-            else
-            {
-                Console.WriteLine("No matches found for the custom regular expression.");
-            }
+            Console.Write("Numbers are :");
+            Print(num1);
+            Console.WriteLine("Print Array without Sort");
+            Print(num1);
+            Stopwatch s2 = new Stopwatch();
+            s2.Start();
+            InsertionSort(num1);
+            s2.Stop();
+            Console.WriteLine("print Array after Insertion sort");
+            Print(num1);
+            Console.WriteLine($"Arraysize is {num1.Length} Time Taken for insertion sort {s2.Elapsed.TotalMilliseconds} milliseconds");
             Console.ReadKey();
-        }
-
-
-        static int CountWords(string inputText)
-        {
-            return inputText.Split(new char[] { ' ', '\t', '\n' }, StringSplitOptions.RemoveEmptyEntries).Length;
-        }
-
-        static List<string> GetEmailAddresses(string inputText)
-        {
-            var emailRegex = new Regex(@"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b");
-            var emailMatches = emailRegex.Matches(inputText);
-
-            var emailAddresses = new List<string>();
-            foreach (Match match in emailMatches)
-            {
-                emailAddresses.Add(match.Value);
-            }
-
-            return emailAddresses;
-        }
-
-        static List<string> ExtractMobileNumbers(string inputText)
-        {
-            var mobileNumberRegex = new Regex(@"\b\d{10}\b");
-            var mobileNumberMatches = mobileNumberRegex.Matches(inputText);
-
-            var mobileNumbers = new List<string>();
-            foreach (Match match in mobileNumberMatches)
-            {
-                mobileNumbers.Add(match.Value);
-            }
-
-            return mobileNumbers;
-        }
-
-        static List<string> PerformCustomRegexSearch(string inputText, string customRegexPattern)
-        {
-            var customRegex = new Regex(customRegexPattern);
-            var customRegexMatches = customRegex.Matches(inputText);
-
-            var matches = new List<string>();
-            foreach (Match match in customRegexMatches)
-            {
-                matches.Add(match.Value);
-            }
-
-            return matches;
 
         }
-
     }
-
 }
-
